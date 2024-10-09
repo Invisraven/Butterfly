f9dd071ac7e502ed03ae314b47597d3e49f84462
Invisraven <43583705+Invisraven@users.noreply.github.com>
2024-10-09 19:57:26 +0200

    0.0.1 Start
    -NewProject

---
+++ Source/Butterfly/Butterfly.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Butterfly.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Butterfly, "Butterfly" );
+ 
---
+++ Source/Butterfly/ButterflyGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ButterflyGameMode.h"
+#include "ButterflyCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AButterflyGameMode::AButterflyGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
